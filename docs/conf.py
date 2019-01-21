cbbdcb3c0b1189be210a1b886b9dac2760260b9e
Michal Klement <miklement@icloud.com>
2019-01-21 20:43:44 +0100

    fixed docs for rtfd

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('/Users/mk/Developer/dota-helper')
+sys.path.append('..')
 
 
 # -- Project information -----------------------------------------------------
